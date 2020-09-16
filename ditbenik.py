bcfa273dcda76457a9ea095f4984c2751c7bb028
TS2656 <70953238+TS2656@users.noreply.github.com>
2020-09-16 13:04:11 +0200

    Add files via upload

---
+++ ditbenik.py
@@ -0,0 +1,36 @@
+Last login: Mon Sep 14 10:11:01 on ttys000
+ts2656@MacBook-Pro-van-Thijn ~ % python3
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("Hello You!, ik ben Thijn")
+Hello You!, ik ben Thijn
+>>> print("Wie ben jij")
+Wie ben jij
+>>> print("Ik ben Timo")
+Ik ben Timo
+>>> print("Hello Timo")
+Hello Timo
+>>> input()
+Timo
+'Timo'
+>>> print("Hello Timo")
+Hello Timo
+>>> print("Ik ben een nieuwkomer op het Mediacollege Amsterdam.")
+Ik ben een nieuwkomer op het Mediacollege Amsterdam.
+>>> print("Door een aantal vragen over mij te beantwoorden leer je mij beter kennen.")
+Door een aantal vragen over mij te beantwoorden leer je mij beter kennen.
+>>> 
+>>> print(" Op welke school zat ik hiervoor.")                                   Op welke school zat ik hiervoor.
+>>> print("A. Fioretti College Hillegom")
+A. Fioretti College Hillegom
+>>> print("B. Nova College Haarlem")
+B. Nova College Haarlem
+>>> print("C. Ichtus College")
+C. Ichtus College
+>>> input()
+A
+'A'
+>>> print("Dat is correct")
+Dat is correct
+>>> 
