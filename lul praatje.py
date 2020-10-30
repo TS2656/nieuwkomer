d08f4dc3fbcadd53d2bda2bcf884c014717edde0
TS2656 <70953238+TS2656@users.noreply.github.com>
2020-10-30 16:45:24 +0100

    Add files via upload

---
+++ lul praatje.py
@@ -0,0 +1,72 @@
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #vraag 1
+print("vraag 1")
+print("Hoe oud is Keeis?")
+print("A : hij is 11")
+print("B : hij is 12")
+print("C : hij is 13)
+antwoord = input()
+if antwoord == "A" :
+    print("dit is fout")
+if antwoord "B" :
+    print("dit is juist")
+if antwoord "C" :
+    print("dit is fout")
+
+#vraag 2
+print("vraag 2")
+print("Wie had Keeis er gemist in de corona tijd?")
+print("A : ze trainer")
+print("B : ze juf")
+print("C : ze zus")
+antwoord = input()
+if antwoord == "A" :
+    print("dit is fout")
+if antwoord "B" :
+    print("dit is juist")
+if antwoord "C" :
+    print("dit is fout")
+
+#vraag 3
+print("vraag 3")
+print("in welke plaats zitten ze in het azc")
+print("A : in rotterdam")
+print("B : in amsterdam")
+print("C : in alkmaar")
+antwoord = input()
+if antwoord == "A" :
+    print("dit is goed")
+if antwoord "B" :
+    print("dit is fout")
+if antwoord "C" :
+    print("dit is fout")
+
+#vraag 4
+print("vraag 4")
+print("wat wil Keeis later voor werk doen in nederland")
+print("A : hij wil later met dieren helpen")
+print("B : hij wil later Youtuber worden")
+print("C : hij wil later op tv komen")
+antwoord = input()
+if antwoord == "A" :
+    print("dit is fout")
+if antwoord "B" :
+    print("dit is goed")
+if antwoord "C" :
+    print("dit is fout")
+
+#vraag 5
+print("vraag 5")
+print("Waar was Keeis niet bang voor")
+print("A : Corona")
+print("B : oorlog")
+print("C : kickboksen")
+antwoord = input()
+if antwoord == "A" :
+    print("dit is goed")
+if antwoord "B" :
+    print("dit is fout")
+if antwoord "C" :
+    print("dit is fout")
